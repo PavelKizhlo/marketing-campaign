51df77e0e41d5e09ce9fa81d6b1826b2641a3b53
Pavel Kizhlo <pavelkizh@gmail.com>
2023-06-06 17:49:28 +0300

    feat: add page header with filter bar

---
+++ src/app/components/filter-bar/filter-bar.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+export interface FilterGroup {
+  name: string;
+  filters?: Filter[];
+}
+
+export interface Filter {
+  name: string;
+}
+
+@Component({
+  selector: 'app-filter-bar',
+  standalone: true,
+  imports: [CommonModule, MatTabsModule, MatButtonModule, MatIconModule],
+  templateUrl: './filter-bar.component.html',
+  styleUrls: ['./filter-bar.component.scss'],
+})
+export class FilterBarComponent {
+  public savedFilterGroups: FilterGroup[] = [
+    { name: 'New Search' },
+    {
+      name: 'Angular',
+      filters: [
+        {
+          name: 'Primary tech stack - any of Angular',
+        },
+        {
+          name: 'Indusrty - any of Some ID',
+        },
+        {
+          name: 'Other filter',
+        },
+      ],
+    },
+    {
+      name: 'React',
+      filters: [
+        {
+          name: 'Primary tech stack - any of React',
+        },
+        {
+          name: 'Indusrty - any of Some ID',
+        },
+      ],
+    },
+  ];
+}
--- src/app/components/pages/marketing-campaign-page/marketing-campaign.component.ts
+++ src/app/components/pages/marketing-campaign-page/marketing-campaign.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FilterBarComponent } from '../../filter-bar/filter-bar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-marketing-campaign',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, FilterBarComponent, MatToolbarModule],
   templateUrl: './marketing-campaign.component.html',
   styleUrls: ['./marketing-campaign.component.scss'],
 })
